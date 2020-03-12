e304cf0eb9509517d81c038130632f591d669ad7
insomnyawolf <insomnyawolf@gmail.com>
2020-03-12 12:59:25 +0100

    Ported to Netcore3.1 Sightly Optimized Updated Gitignore

---
+++ FileLoader.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace HDtoSD
+{
+    enum ImageFormat
+    {
+        Png,
+        Jpg
+    }
+    class FileLoader
+    {
+        public string FolderPath { get; set; }
+        public string FilterPattern { get; set; }
+        public ImageFormat[] ImageFormat { get; set; }
+
+        public FileLoader(string FolderPath, string FilterPattern, params ImageFormat[] ImageFormat)
+        {
+            this.FolderPath = FolderPath;
+            this.FilterPattern = FilterPattern;
+            this.ImageFormat = ImageFormat;
+        }
+
+        //Returns an array containing the HD image files in the skin folder
+        public List<string> Load()
+        {
+            List<string> hdFiles = new List<string>();
+
+            foreach (var filter in ImageFormat)
+            {
+                string[] filesFound = Directory.GetFiles(FolderPath, string.Format("*.{0}", filter), SearchOption.TopDirectoryOnly);
+                for (int i = 0; i < filesFound.Length; i++)
+                {
+                    //Checks if it's an HD file
+                    if (filesFound[i].Contains(FilterPattern))
+                    {
+                        hdFiles.Add(filesFound[i]);
+                    }
+                }
+            }
+            return hdFiles;
+        }
+    }
+}
